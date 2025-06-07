ea5a0d861ca0c35ae77b49cd32b405a9d8c2e4c7
Nandhakumar <nknandhakumar64@gmail.com>
2025-06-07 19:03:52 +0530

    Sum of digits

--- src/com/Nk/GFG/Journey/Recursion/FindSumOfDigits.java
+++ src/com/Nk/GFG/Journey/Recursion/FindSumOfDigits.java
@@ -1,19 +1,17 @@
 package com.Nk.GFG.Journey.Recursion;
 
-public class SumOfDigits {
-    // Sum Of Digits
+public class FindSumOfDigits {
+    // Sum of Digits of a Number Using Recursion
     // https://www.geeksforgeeks.org/problems/sum-of-digits1742/1
     public static void main(String[] args) {
         int n = 12;
-        int sum = 0;
         System.out.println(sumOfDigits(n));
     }
-    static int sumOfDigits(int n) {
+    static int sumOfDigits(int n){
         // base case
         if (n==0){
-            return 0 ;
+            return 0;
         }
-        // recursive call
         return (n%10) + sumOfDigits(n/10);
     }
 }
