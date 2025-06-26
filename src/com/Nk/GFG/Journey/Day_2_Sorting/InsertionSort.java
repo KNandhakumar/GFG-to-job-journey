06bc80cfaebb3fe275fe61843a23d485684f95df
Nandhakumar <nknandhakumar64@gmail.com>
2025-06-26 13:22:17 +0530

    Day-2-Sorting : insertion sort

---
+++ src/com/Nk/GFG/Journey/Day_2_Sorting/InsertionSort.java
@@ -0,0 +1,27 @@
+package com.Nk.GFG.Journey.Day_2_Sorting;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    // Insertion sort
+    // https://www.geeksforgeeks.org/problems/insertion-sort/1
+    public static void main(String[] args) {
+        int[] arr = {4, 1, 3, 9, 7};
+        insertionSort(arr);
+    }
+    // time complexity O(n2)
+    // space complexity O(1)
+    public static void insertionSort(int[] arr) {
+        // code here
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i-1;
+            while (j>=0 && arr[j]>key){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
